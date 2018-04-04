25a4bac89a0634e1d2a45d360efb3d74321cb965
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2018-04-04 10:35:40 -0300

    upload de arquivos publicos do template (js e css)

--- application/layouts/default.php
+++ application/layouts/default.php
@@ -1,38 +1,34 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
-<head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>{title_for_layout}</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="<?PHP echo CSSPATH; ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?PHP echo CSSPATH; ?>vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+        <link href="<?PHP echo CSSPATH; ?>dist/css/sb-admin-2.css" rel="stylesheet">
+        <link href="<?PHP echo CSSPATH; ?>vendor/morrisjs/morris.css" rel="stylesheet">
+        <link href="<?PHP echo CSSPATH; ?>vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>vendor/jquery/jquery.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>vendor/metisMenu/metisMenu.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>vendor/raphael/raphael.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>vendor/morrisjs/morris.min.js"></script>
+        <script src="<?PHP echo JSPATH; ?>data/morris-data.js"></script>
+        <script src="<?PHP echo JSPATH; ?>dist/js/sb-admin-2.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js"></script>
-
-<title>{title_for_layout}</title>
-
-{css_for_layout}
-            {js_for_layout}
-
-</head>
-<body>
-<div id="geral">
-
-<div id="meio">
-
-{content_for_layout}
-
-</div>
-
-</div>
-<script type="text/javascript"> var basepath = '<?php print(base_url()); ?>'; </script>
-</body>
+        {css_for_layout}
+        {js_for_layout}
+    </head>
+    <body>
+        <div id="geral">
+            <div id="meio">
+                {content_for_layout}
+            </div>
+        </div>
+        <script type="text/javascript"> var basepath = '<?php print(base_url()); ?>'; </script>
+    </body>
 </html>
