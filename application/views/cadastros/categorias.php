0f26e6a88b5e01f119e68acb33e243104b01eb95
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2018-04-05 16:38:12 -0300

    botão de nova categoria

--- application/views/cadastros/categorias.php
+++ application/views/cadastros/categorias.php
@@ -1,31 +1,42 @@
 <h1 class="page-header">Categorias</h1>
 
-<table class="table table-striped table-bordered table-hover">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nome</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-<?php
-    foreach ($listaCategorias as $key => $value) { ?>
-        <tr>
-            <th scope="row"><?php echo $value['id']; ?></th>
-            <td><?php echo $value['nome']; ?></td>
-            <td>
-                <a href="/cadastros/categorias/editar/<?php echo $value['id']; ?>"><img src="<?php echo IMGPATH; ?>if_Modify_132500.png"></a>
-                <a href="/cadastros/categorias/excluir/<?php echo $value['id']; ?>"><img src="<?php echo IMGPATH; ?>if_Close_132419.png"></a>
-            </td>
-        </tr>
-<?php } ?>
-    </tbody>
-</table>
-
-<?php
-if($retorno){ ?>
-    <div class="alert alert-success alert-dismissable">
-        Categoria removida com sucesso!
+<div class="row">
+    <div class="col-md-12">
+        <a href="" class="btn btn-primary">Nova</a>
     </div>
-<?php } ?>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+            foreach ($listaCategorias as $key => $value) { ?>
+                <tr>
+                    <th scope="row"><?php echo $value['id']; ?></th>
+                    <td><?php echo $value['nome']; ?></td>
+                    <td>
+                        <a href="/cadastros/categorias/editar/<?php echo $value['id']; ?>"><img src="<?php echo IMGPATH; ?>if_Modify_132500.png"></a>
+                        <a href="/cadastros/categorias/excluir/<?php echo $value['id']; ?>"><img src="<?php echo IMGPATH; ?>if_Close_132419.png"></a>
+                    </td>
+                </tr>
+        <?php } ?>
+            </tbody>
+        </table>
+
+        <?php
+        if($retorno){ ?>
+            <div class="alert alert-success alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                Categoria removida com sucesso!
+            </div>
+        <?php } ?>
+    </div>
+</div>
